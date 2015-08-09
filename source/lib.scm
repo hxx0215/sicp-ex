2c66354145bafd9cca4ce92685b95d4c72141ee8
hxx0215 <hxx0215@126.com>
2015-08-09 21:27:41 +0800

    add floodlight && flatmap

---
+++ source/lib.scm
@@ -0,0 +1,11 @@
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+    initial
+    (op (car sequence)
+        (accumulate op initial (cdr sequence))
+        )
+    )
+  )
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq))
+  )
