b4f8e70cf9d659668c056032dc6ad8747887572b
hxx0215 <hxx0215@126.com>
2015-08-21 21:29:21 +0800

    shanana encode

--- source/huffman.scm
+++ source/huffman.scm
@@ -131,3 +131,10 @@
   )
 
 (generate-huffman-tree sample-pairs)
+
+(define rock-alphabet (list (list 'A 2) (list 'NA 16) (list 'BOOM 1) (list 'SHA 3) (list 'GET 2) (list 'YIP 9) (list 'JOB 2) (list 'WAH 1)))
+
+(generate-huffman-tree rock-alphabet)
+(define rock-message '(Get a job Sha na na na na na na na na Get a job Sha na na na na na na na na Wah yip yip yip yip yip yip yip yip yip Sha boom))
+
+(encode rock-message (generate-huffman-tree rock-alphabet))
