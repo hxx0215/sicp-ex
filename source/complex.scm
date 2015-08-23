134f3adfa9a622fb2d086b5093048b6928028802
hxx0215 <hxx0215@126.com>
2015-08-23 16:23:14 +0800

    complex

--- source/complex.scm
+++ source/complex.scm
@@ -1,1 +1,3 @@
-(define (make-from-real-imag (real-partc z) (imag-partc z)))
+(define (add-complex z1 z2)
+  (make-from-real-imag (+ (real-part z1) (real-part ze)) (+ (imag-part z1) (imag-part z2)))
+  )
