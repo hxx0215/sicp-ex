5d9c6278f62387da814ecdfcc1bf2b36721273c4
hxx0215 <hxx0215@126.com>
2015-08-31 21:44:53 +0800

    the little scheme

--- source/thelittlescheme.scm
+++ source/thelittlescheme.scm
@@ -22,3 +22,32 @@
 (lat? (list 'Jack '('Sprat 'could) 'eat 'no 'chicken 'fat))
 
 (lat? '())
+
+(define (rember a lat)
+  (if (null? lat)
+    '()
+    (if (eq? a (car lat))
+      (cdr lat)
+      (cons (car lat) (rember a (cdr lat)))
+      )
+    )
+  )
+
+(rember 'mint '(lamb chops and mint jelly))
+
+(rember 'mint '(lamb chops and mint flavored mint jelly))
+
+(rember 'toast '(bacon lettuce and tomato))
+
+(define (firsts l)
+  (if (null? l)
+    '()
+    (cons () (firsts (cdr l)))
+    )
+  )
+
+(firsts '((apple peach pumpkin)
+        (plum pear cherry)
+        (grape raisin pea)
+        (bean carrot eggplant)
+        ))
