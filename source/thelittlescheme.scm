32ac973df24f83e89f0775a8669fbed9642e652f
hxx0215 <hxx0215@126.com>
2015-09-04 16:25:21 +0800

    littlescheme

--- source/thelittlescheme.scm
+++ source/thelittlescheme.scm
@@ -200,3 +200,188 @@
     )
   )
 
+(define tup+
+  (lambda (tup1 tup2)
+    (if (null? tup1)
+        tup2
+      (if (null? tup2)
+          tup1
+        (cons (+ (car tup1) (car tup2))
+              (tup+ (cdr tup1) (cdr tup2))
+              )
+        )
+      )
+    )
+  )
+
+(tup+ (list 3 6 9 11 4)
+      (list 8 5 2 0 7)
+      )
+
+(tup+ (list 2 3)
+      (list 4 6)
+      )
+
+(tup+ (list 3 7)
+      (list 4 6 8 1)
+      )
+
+(tup+ (list 3 7 8 1)
+      (list 4 6)
+      )
+
+(define small
+  (lambda (a b)
+    (if (zero? b)
+      #f
+      (if (zero? a)
+        #t
+        (small (sub1 a) (sub1 b))
+        )
+      )
+    )
+  )
+
+(define ^
+  (lambda (a b)
+    (if (zero? b)
+      1
+      (* a (^ a (sub1 b)))
+      )
+    )
+  )
+
+(define div
+  (lambda (a b)
+    (if (small a b)
+      0
+      (add1 (div (- a b) b))
+      )
+    )
+  )
+
+(div 16 4)
+
+(define length-lat 
+  (lambda (lat)
+    (if (null? lat)
+      0
+      (add1 (length-lat (cdr lat)))
+      )
+    )
+  )
+
+(length-lat '(hotdogs with mustard sauerkraut and pickles))
+
+(length-lat '(ham and chees on rye))
+
+(define pick
+  (lambda (n lat)
+    (if (null? lat)
+      '()
+      (if (zero? (sub1 n))
+        (car lat)
+        (pick (sub1 n) (cdr lat))
+        )
+      )
+    )
+  )
+
+(pick 4 '(lasagna spaghetti ravioli macaroni meatball))
+
+(define rempick
+  (lambda (n lat)
+    (if (null? lat)
+      '()
+      (if (zero? (sub1 n))
+        (cdr lat)
+        (cons (car lat) (rempick (sub1 n) (cdr lat)))
+        )
+      )
+    )
+  )
+
+(rempick 3 '(hotdogs with hot mustard))
+
+(define no-nums
+  (lambda (lat)
+    (if (null? lat)
+      '()
+      (if (number? (car lat))
+        (no-nums (cdr lat))
+        (cons (car lat) (no-nums (cdr lat)))
+        )
+      )
+    )
+  )
+
+(no-nums '(5 pears 6 prunes 9 dates))
+
+(define eqan?
+  (lambda (a b)
+    (if (and (number? a) (number? b))
+      (= a b)
+      (if (or (number? a) (number? b))
+        false
+        (eq? a b)
+        )
+      )
+    )
+  )
+
+(define occur
+  (lambda (a lat)
+    (if (null? lat)
+      0
+      (if (eq? a (car lat))
+        (add1 (occur a (cdr lat)))
+        (occur a (cdr lat))
+        )
+      )
+    )
+  )
+
+(occur 'test '(test test test test))
+
+(define rember*
+  (lambda (a l)
+    (if (null? l)
+      '()
+      (if (atom? (car l))
+        (if (eq? a (car l))
+          (rember* a (cdr l))
+          (cons (car l) (rember* a (cdr l)))
+          )
+       (cons (rember* a (car l)) (rember* a (cdr l)))
+        )
+      )
+    )
+  )
+
+(rember* 'sauce '(((tomato sauce))
+                  ((bean) sauce)
+                  (and ((flying)) sauce)
+                  ))
+
+(define insertR*
+  (lambda (new old l)
+    (if (null? l)
+        '()
+      (if (atom? (car l))
+          (if (eq? old (car l))
+              (cons old (cons new (insertR* new old (cdr l))))
+            (cons (car l) (insertR* new old (cdr l)))
+            )
+        (cons (insertR* new old (car l)) (insertR* new old (cdr l)))
+        )
+      )
+    )
+  )
+
+(insertR* 'roast 'chuck '((how much (wood))
+                        could
+                        ((a (wood) chuck))
+                        (((chuck)))
+                        (if (a) ((wood chuck)))
+                        could chuck wood
+                        ))
