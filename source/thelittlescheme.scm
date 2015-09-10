68bf6a9d51726ff59fadf2873fb8c47de29f0319
hxx0215 <hxx0215@126.com>
2015-09-10 17:41:23 +0800

    little scheme

--- source/thelittlescheme.scm
+++ source/thelittlescheme.scm
@@ -1090,3 +1090,69 @@
  '(a b c))
 
 (Y (Y Y))
+
+(define new-entry build)
+
+(define lookup-in-entry
+  (lambda (name entry entry-f)
+    (lookup-in-entry-help name
+                          (first entry)
+                          (second entry)
+                          entry-f
+                          )
+    )
+  )
+(define lookup-in-entry-help
+  (lambda (name names values entry-f)
+    (cond
+      ((null? names) (entry-f name))
+      ((eq? name (car names)) (car values))
+      (else
+        (lookup-in-entry-help 
+          name
+          (cdr names)
+          (cdr values)
+          entry-f
+          )
+        )
+      )
+    )
+  )
+
+(define extend-table cons)
+
+(define lookup-in-table 
+  (lambda (name table table-f)
+    (cond
+      ((null? table) (table-f name))
+      (else
+        (lookup-in-entry name
+                         (car table)
+                         (lambda (name)
+                           (lookup-in-table
+                                      name
+                                     (cdr table) 
+                                     table-f
+                                      )
+                           )
+                         )
+        )
+      )
+    )
+  )
+
+(define expression-to-action
+  (lambda (e)
+    (cond
+      ((atom? e) (atom-to-action e))
+      (else (list-to-action e))
+      )
+    )
+  )
+(define atom-to-action 
+  (lambda (e)
+    (cond
+      ((number? e) *const)
+      )
+    )
+  )
